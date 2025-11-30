059bcb403ae0dcea595e55a202a9db1899d92040
David Edwards <david.l.edwards@gmail.com>
2025-11-29 22:38:04 -0600

    update Boyer-Moore search with good suffix shift table

--- src/search.rs
+++ src/search.rs
@@ -54,14 +54,65 @@ struct TermPattern {
     /// A patternized version of [`term`](Self::term).
     pattern: Vec<char>,
 
-    /// The shift table used by Boyer-Moore.
-    shift: HashMap<char, usize>,
+    /// Bad character shift table maps characters in `pattern` to their rightmost
+    /// position.
+    bc_shift: HashMap<char, usize>,
+
+    /// Good suffix shift table determines how far to shift based on position in
+    /// `pattern`.
+    gs_shift: Vec<usize>,
 
     /// Indicates the sensitivity of case.
     case_strict: bool,
 }
 
 impl TermPattern {
+    fn build_bc_shift(pattern: &[char]) -> HashMap<char, usize> {
+        let len = pattern.len();
+        pattern
+            .iter()
+            .enumerate()
+            .take(len.saturating_sub(1))
+            .map(|(i, c)| (*c, i))
+            .collect()
+    }
+
+    fn build_gs_shift(pattern: &[char]) -> Vec<usize> {
+        let len = pattern.len();
+        if len > 0 {
+            let mut gs_shift = vec![len; len];
+            let mut border = vec![0; len + 1];
+            border[len] = len + 1;
+            let mut i = len;
+            let mut j = len + 1;
+            while i > 0 {
+                // Find border of pattern[i..pat_len]
+                while j <= len && pattern[i - 1] != pattern[j - 1] {
+                    // If good_suffix[j-1] hasn't been set yet, set it
+                    if gs_shift[j - 1] == len {
+                        gs_shift[j - 1] = j - i;
+                    }
+                    j = border[j];
+                }
+                i -= 1;
+                j -= 1;
+                border[i] = j;
+            }
+            j = border[0];
+            for i in 0..len {
+                if gs_shift[i] == len {
+                    gs_shift[i] = j;
+                }
+                if i == j {
+                    j = border[j];
+                }
+            }
+            gs_shift
+        } else {
+            vec![]
+        }
+    }
+
     pub fn new(term: &str, case_strict: bool) -> TermPattern {
         // Pattern is downcased when case-sensitivity is relaxed, otherwise
         // it is faithful represention of term.
@@ -76,48 +127,41 @@ impl TermPattern {
             })
             .collect::<Vec<_>>();
 
-        // Shift table reflects case-sensitivity as well.
-        let mut shift = HashMap::new();
-        for (i, c) in pattern.iter().enumerate() {
-            let c = if case_strict {
-                *c
-            } else {
-                c.to_ascii_lowercase()
-            };
-            shift.insert(c, i);
-        }
+        let bc_shift = Self::build_bc_shift(&pattern);
+        let gs_shift = Self::build_gs_shift(&pattern);
 
         TermPattern {
             term: term.to_string(),
             pattern,
-            shift,
+            bc_shift,
+            gs_shift,
             case_strict,
         }
     }
 
     fn search(&self, buffer: &Buffer, pos: usize) -> Option<(usize, usize)> {
-        let pat_len = self.pattern.len();
-        if pat_len > 0 && pos + pat_len <= buffer.size() {
-            let stop_pos = buffer.size() - pat_len;
+        let len = self.pattern.len();
+        if len > 0 && pos + len <= buffer.size() {
+            let stop_pos = buffer.size() - len;
             let mut pos = pos;
             while pos <= stop_pos {
                 // Pattern matching occurs right-to-left.
-                let mut i = pat_len;
+                let mut i = len;
                 while i > 0 && self.pattern[i - 1] == self.buf_at(buffer, pos + i - 1) {
                     i -= 1;
                 }
                 if i == 0 {
                     // Pattern matched.
-                    return Some((pos, pos + pat_len));
+                    return Some((pos, pos + len));
                 } else {
-                    // Pattern match failed, so move position forward based on shift
-                    // table and continue searching.
-                    i -= 1;
-                    pos += self
-                        .shift
-                        .get(&self.buf_at(buffer, pos + i))
-                        .map(|n| if *n > i { 1 } else { i - n })
-                        .unwrap_or(i + 1);
+                    let bc = self.buf_at(buffer, pos + i - 1);
+                    let bc_shift = if let Some(&p) = self.bc_shift.get(&bc) {
+                        if p < i - 1 { i - 1 - p } else { 1 }
+                    } else {
+                        i
+                    };
+                    let gs_shift = self.gs_shift[i - 1];
+                    pos += bc_shift.max(gs_shift);
                 }
             }
             // At this point, buffer was exhausted without match.
@@ -165,7 +209,7 @@ impl Pattern for TermPattern {
 ///
 /// The most efficient method of search is [`find_str()`](Pattern::find_str) because
 /// the algorithm only works directly with `&str` types. Using [`find()`](Pattern::find)
-/// requires an intermediate converstion from [`Buffer`] to `&str`.
+/// requires an intermediate conversion from [`Buffer`] to `&str`.
 struct RegexPattern {
     regex: Regex,
 }
@@ -212,5 +256,131 @@ impl Pattern for RegexPattern {
                 None
             }
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use regex_lite::RegexBuilder;
+
+    const TEXT: &str = "The quick brown fox jumps over the lazy dog";
+
+    #[test]
+    fn search_term_normal() {
+        let pattern = TermPattern::new("BrOwN FoX", false);
+        let found = pattern.find_str(TEXT, 0).unwrap();
+        assert_eq!(found, (10, 19));
+    }
+
+    #[test]
+    fn search_term_normal_at_end() {
+        let pattern = TermPattern::new("DOG", false);
+        let found = pattern.find_str(TEXT, 0).unwrap();
+        assert_eq!(found, (40, 43));
+    }
+
+    #[test]
+    fn search_term_normal_not_found() {
+        let pattern = TermPattern::new("jumpz", false);
+        let found = pattern.find_str(TEXT, 0);
+        assert_eq!(found, None);
+    }
+
+    #[test]
+    fn search_term_case() {
+        let pattern = TermPattern::new("jump", true);
+        let found = pattern.find_str(TEXT, 0).unwrap();
+        assert_eq!(found, (20, 24));
+    }
+
+    #[test]
+    fn search_term_case_at_end() {
+        let pattern = TermPattern::new("dog", false);
+        let found = pattern.find_str(TEXT, 0).unwrap();
+        assert_eq!(found, (40, 43));
+    }
+
+    #[test]
+    fn search_term_case_not_found() {
+        let pattern = TermPattern::new("Jump", true);
+        let found = pattern.find_str(TEXT, 0);
+        assert_eq!(found, None);
+    }
+
+    #[test]
+    fn search_regex_normal() {
+        let pattern = RegexPattern::new(build_regex_normal("qu[A-Z]+\\s*.+wN"));
+        let found = pattern.find_str(TEXT, 0).unwrap();
+        assert_eq!(found, (4, 15))
+    }
+
+    #[test]
+    fn search_regex_normal_at_end() {
+        let pattern = RegexPattern::new(build_regex_normal("LazY.*$"));
+        let found = pattern.find_str(TEXT, 0).unwrap();
+        assert_eq!(found, (35, 43))
+    }
+
+    #[test]
+    fn search_regex_normal_not_found() {
+        let pattern = RegexPattern::new(build_regex_normal("qu[a-z]+\\s.+nw"));
+        let found = pattern.find_str(TEXT, 0);
+        assert_eq!(found, None);
+    }
+
+    #[test]
+    fn search_regex_case() {
+        let pattern = RegexPattern::new(build_regex_case("qu[a-z]+\\s*.+wn"));
+        let found = pattern.find_str(TEXT, 0).unwrap();
+        assert_eq!(found, (4, 15))
+    }
+
+    #[test]
+    fn search_regex_case_at_end() {
+        let pattern = RegexPattern::new(build_regex_case("lazy.*$"));
+        let found = pattern.find_str(TEXT, 0).unwrap();
+        assert_eq!(found, (35, 43))
+    }
+
+    #[test]
+    fn search_regex_case_not_found() {
+        let pattern = RegexPattern::new(build_regex_case("qu[A-Z]+\\s.+wn"));
+        let found = pattern.find_str(TEXT, 0);
+        assert_eq!(found, None);
+    }
+
+    #[test]
+    fn search_empty_buffer() {
+        let pattern = TermPattern::new("anything", false);
+        let found = pattern.find_str("", 0);
+        assert_eq!(found, None);
+
+        let pattern = RegexPattern::new(build_regex_normal("any.+thing"));
+        let found = pattern.find_str("", 0);
+        assert_eq!(found, None);
+    }
+
+    #[test]
+    fn search_with_empty_term() {
+        let pattern = TermPattern::new("", false);
+        let found = pattern.find_str(TEXT, 0);
+        assert_eq!(found, None);
+    }
+
+    fn build_regex_normal(term: &str) -> Regex {
+        build_regex(term, false)
+    }
+
+    fn build_regex_case(term: &str) -> Regex {
+        build_regex(term, true)
+    }
+
+    fn build_regex(term: &str, case_strict: bool) -> Regex {
+        RegexBuilder::new(term)
+            .case_insensitive(!case_strict)
+            .build()
+            .ok()
+            .unwrap()
     }
 }
